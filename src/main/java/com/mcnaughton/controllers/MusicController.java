92d5c570b63f34322613eb1c81e82b3896fdb568
Ian McNaughton <Ian McNaughton>
2017-06-02 00:43:29 -0700

    cleaned up import statments

--- src/main/java/com/mcnaughton/controllers/MusicController.java
+++ src/main/java/com/mcnaughton/controllers/MusicController.java
@@ -2,17 +2,13 @@ package com.mcnaughton.controllers;
 
 import com.mcnaughton.client.spotifyModels.response.Playlist;
 import com.mcnaughton.service.MusicService;
-import com.sun.org.apache.regexp.internal.RE;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import twitter4j.TwitterException;
 
 @RestController
 @RequestMapping(value = "/api/music/")
