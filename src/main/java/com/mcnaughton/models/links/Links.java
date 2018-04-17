60f707803a58fa3c60b3e16c773b92b92dfc7d21
Ian McNaughton <IanMcNaugh@gmail.com>
2018-04-17 09:26:24 -0500

    updating links

--- src/main/java/com/mcnaughton/models/links/Links.java
+++ src/main/java/com/mcnaughton/models/links/Links.java
@@ -3,6 +3,9 @@ package com.mcnaughton.models.links;
 public class Links {
     private String github = "https://github.com/imcnaugh";
     private String bestPizzaEver = "https://www.google.com/maps/place/Finn's+Pizza/@59.6020014,-151.4233406,16z/data=!4m5!3m4!1s0x0:0x48f58e1f7b85ad11!8m2!3d59.6021053!4d-151.4235735";
+    private String personalBitbucket = "2484943748.com:7990";
+    private String personalJenkins = "2484943748.com:8081";
+    private String personalJira = "2484943748.com";
 
     public String getGithub() {
         return github;
@@ -10,5 +13,17 @@ public class Links {
 
     public String getBestPizzaEver() {
         return bestPizzaEver;
+    }
+
+    public String personalBitbucket() {
+        return personalBitbucket;
+    }
+
+    public String personalJenkins() {
+        return personalJenkins;
+    }
+
+    public String personalJira() {
+        return personalJira;
     }
 }
